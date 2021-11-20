9d3cca7a0cac53a9e365b21bfc2edab46612a090
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-11-20 13:48:28 -0500

    13.5

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,6 +16,14 @@ router.get('/', (req, res) => {
     ],
     order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -44,7 +53,12 @@ router.get('/:id', (req, res) => {
       {
         model: User,
         attributes: ['username']
+      },
+      { model: Comment,
+        attributes: []
+
       }
+    
     ]
   })
     .then(dbPostData => {
